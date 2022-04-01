021cb53c2c46bbdbcebc8f86e40818ac172ec7f1
Nicole Magpantay <20709370+magpantaynic@users.noreply.github.com>
2022-04-01 02:21:45 -0400

    Create SimpleCalculator.java

---
+++ SimpleCalculator.java
@@ -0,0 +1,37 @@
+/*
+        Design a class (no main method) called SimpleCalculator that performs basic math operations 
+        (addition, subtraction, multiplication, division) on two operands.
+*/
+
+
+public class SimpleCalculator {
+
+    //purpose of private is just for something that "the car doesn't need to know"
+    private double num1, num2;
+
+    public SimpleCalculator()
+    {
+
+        // Addition Method +
+        
+
+        // Subtraction Method -
+
+        // Multiplication Method *
+
+        // Division Method /
+
+    }
+    public double getNum1() {
+        return num1;
+    }
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+    public double getNum2() {
+        return num2;
+    }
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+}
